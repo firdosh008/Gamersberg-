4029bcff0e10ed26e0e32210ea04491b594aac42
ahmadfirdosh008 <ahmadfirdosh008@gmail.com>
2025-02-28 20:49:16 +0530

    resposnive

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col bg-gradient-custom">
       <Navbar />
-      <div className="flex-grow flex flex-col items-center justify-center py-8 px-4">
+      <div className="flex-grow flex flex-col items-center justify-center py-8 ">
         {/* Responsive Background & Header Section */}
         <div className="relative w-full bg-calculator bg-no-repeat bg-left bg-contain lg:h-[392px] md:h-[300px] h-[200px] flex items-center justify-start">
           <div className="ml-10 sm:ml-20 md:ml-32 lg:ml-40">
